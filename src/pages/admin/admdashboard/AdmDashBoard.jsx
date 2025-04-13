e02d2b7d4ba9da1524f13e4090afbb858460a5d8
soarxbit <138611070+soarxbit@users.noreply.github.com>
2025-04-13 21:07:10 +0530

    admin with date on off

--- src/pages/admin/admdashboard/AdmDashBoard.jsx
+++ src/pages/admin/admdashboard/AdmDashBoard.jsx
@@ -7,24 +7,41 @@ import { Button } from "primereact/button";
 export const AdmDashBoard = () => {
   const url = process.env.REACT_APP_HOST_ADDR;
   const apikey = process.env.REACT_APP_APIKEY;
+  const [sdate, setSdate] = useState("2025-01-01")
+  const [edate, setEdate] = useState(null)
+  const [allow, setAllow] = useState(0)
   const [disDate, setDisDate] = useState({
-    startdata:"",
-    enddata:""
+    startdata:null,
+    enddata:null,
+    is_allowed:0
   })
   useEffect(()=>{
     const fetchData = async () => {
-      const resp = await axios.get(url + "/admin/getactwitdate", {
+      const resp = await axios.get(url + "/admin/getactwithdate", {
         headers: {
           "x-api-key": apikey,
         },
       });
       if (resp.status === 200) {
         //   setBalance(resp.data.balance);
-        setDisDate(resp.data.withdraws);
+        setDisDate({startdata:resp.data.sdate, enddata:resp.data.edate, is_allowed:resp.data.is_allowed});
+        setSdate(resp.data.sdate);
+        setEdate(resp.data.edate,);
+        setAllow(resp.data.is_allowed);
       }
     };
     fetchData();
   },[])
+  const handleSubmit = async () =>{
+    const resp = await axios.post(url + "/admin/setactwithdate",{sdate, edate}, {
+      headers: {
+        "x-api-key": apikey,
+      },
+    });
+    if(resp.status===200){
+      alert("Submited")
+    }
+  }
   return (
     <div className="admdashboard">
       <div className="hero">
@@ -35,14 +52,14 @@ export const AdmDashBoard = () => {
                 <div className="form-group">
                   <div className="component">
                     <div>START DATE</div>
-                    <InputText type="date" className="p-inputtext-sm" />
+                    <InputText type="date" value={sdate} className="p-inputtext-sm" onChange={e=>setSdate(e.target.value)} />
                   </div>
                   <div className="component">
-                    <div>START DATE</div>
-                    <InputText type="date" className="p-inputtext-sm" />
+                    <div>FROM DATE</div>
+                    <InputText type="date" value={edate} className="p-inputtext-sm" onChange={e=>setEdate(e.target.value)} />
                   </div>
                   <div className="component">
-                    <Button label="Confirm" />
+                    <Button label="Confirm" type="button" onClick={handleSubmit} />
                   </div>
                 </div>
                 
